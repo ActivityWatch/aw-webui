8e929a5ee025cb53d052b98e2619c8023fb43a01
Erik Bjäreholt <erik.bjareholt@gmail.com>
2016-07-19 01:14:27 +0200

    Minor progress on the Bucket page

--- src/Buckets.vue
+++ src/Buckets.vue
@@ -14,12 +14,30 @@ export default {
   name: "Buckets",
   data() {
     return {
+      $Bucket: this.$resource('/api/0/buckets{/id}'),
       buckets: [
           {name: "test", length: 1337},
           {name: "another_test", length: 2},
           {name: "or_rather_a_mock", length: 100}
-      ]
+      ],
     }
+  },
+  methods: {
+    getBuckets: function() {
+      $Bucket.get().then((response) => {
+          this.$set('buckets', response.json())
+        });
+    },
+
+    getBucketInfo: function(bucket_id) {
+      $Bucket.get({"id": "bucket_id"}).then((response) => {
+        this.$add('buckets', response.json())
+      });
+    }
+  },
+  ready: function() {
+    this.getBuckets();
+    this.getBucketInfo("test");
   }
 }
 </script>
