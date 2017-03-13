4d9b3f29e2c19fb3c36b19c0a06cc69e658962c9
Erik Bjäreholt <erik.bjareholt@gmail.com>
2017-03-13 20:49:57 +0100

    Removed and replaced some trickery

--- src/visualizations/summary.js
+++ src/visualizations/summary.js
@@ -12,14 +12,6 @@ function renderSummary(el, apps) {
   // Clear element
   el.innerHTML = "";
 
-  // Function for hover animation
-  var script = document.createElement("script");
-  script.text =  'function set_color(elem_id, color) {'
-  script.text += '  var rect = document.getElementById(elem_id).children[0];'
-  script.text += '  rect.style.fill = color;'
-  script.text += '};'
-  el.appendChild(script);
-
   let svg = d3.select(el).append("svg");
   svg.attr("width", "100%");
 
@@ -27,8 +19,6 @@ function renderSummary(el, apps) {
   var longest_duration = apps[0].duration
   _.each(apps, function(app, i) {
     // TODO: Expand on click and list titles
-    let eg = svg.append("g")
-      .attr("id", "summary_app_"+i);
 
     // Variables
     var width = (app.duration/longest_duration)*80+"%";
@@ -36,6 +26,12 @@ function renderSummary(el, apps) {
     let textSize = 15;
     var appcolor = color.getAppColor(app.name);
     var hovercolor = Color(appcolor).darken(0.4).rgbString();
+
+    // The group representing an application in the barchart
+    let eg = svg.append("g");
+    eg.attr("id", "summary_app_"+i)
+      .on("mouseover", function() { eg.select("rect").style("fill", hovercolor) })
+      .on("mouseout", function() { eg.select("rect").style("fill", appcolor) });
 
     // Color box background
     eg.append("rect")
@@ -45,7 +41,7 @@ function renderSummary(el, apps) {
      .attr("ry", 5)
      .attr("width", width)
      .attr("height", barHeight)
-     .style("fill", appcolor)
+     .style("fill", appcolor);
 
     // App name
     eg.append("text")
@@ -64,16 +60,6 @@ function renderSummary(el, apps) {
      .attr("font-family", "sans-serif")
      .attr("font-size", textSize + "px")
      .attr("fill", "black")
-
-    // Invisible hover animaiton rect
-    eg.append("rect")
-     .attr("x", 0)
-     .attr("y", curr_y)
-     .attr("width", "100%")
-     .attr("height", barHeight)
-     .style("fill", "rgba(1, 1, 1, 0)")
-     .attr("onmouseover", "set_color('summary_app_"+i+"', '"+hovercolor+"');")
-     .attr("onmouseout", "set_color('summary_app_"+i+"', '"+appcolor+"')");
 
     curr_y += barHeight + 5;
   });
