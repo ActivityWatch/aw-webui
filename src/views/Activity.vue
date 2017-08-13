84865dc6d3ca7f8caab0c5364df7b72d040ce6ad
Erik Bjäreholt <erik@bjareho.lt>
2017-08-13 13:08:44 +0200

    added a whole bunch of stuff

--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -39,6 +39,12 @@ div
 
   hr
 
+  h4 Window titles #[b-button(size="sm" v-on:click="numberOfWindowTitles += 5; queryWindowTitles()") Show 5 more]
+
+  div#windowtitles-container
+
+  hr
+
   h4 Timeline
 
   div#apptimeline-container
@@ -78,6 +84,7 @@ import 'vue-awesome/icons/refresh'
 let $QueryView  = Resources.$QueryView;
 let $CreateView  = Resources.$CreateView;
 let $Info  = Resources.$Info;
+let $Event  = Resources.$Event;
 
 var daylength = 86400000;
 
@@ -91,6 +98,7 @@ export default {
       duration: "",
       eventcount: 0,
       errormsg: "",
+      numberOfWindowTitles: 5
     }
   },
 
@@ -116,10 +124,7 @@ export default {
       return this.$route.params.date || new Date().toISOString();
     },
     datestart: function() {
-      // Returns a
-      console.log(this.date);
       let datestart = moment(this.date).startOf('day').format();
-      console.log(datestart);
       return datestart;
     },
     dateShort: function() {
@@ -132,6 +137,9 @@ export default {
     // Create summary
     var summary_elem = document.getElementById("appsummary-container")
     summary.create(summary_elem);
+
+    var titles_elem = document.getElementById("windowtitles-container")
+    summary.create(titles_elem);
 
     // Create timeline
     var timeline_elem = document.getElementById("apptimeline-container")
@@ -161,71 +169,91 @@ export default {
       this.errormsg = "";
 
       if (PRODUCTION){
-        var window_bucket_name = "aw-watcher-window_" + this.host;
-        var afk_bucket_name = "aw-watcher-afk_" + this.host;
+        this.window_bucket_name = "aw-watcher-window_" + this.host;
+        this.afk_bucket_name = "aw-watcher-afk_" + this.host;
       } else {
         console.log("Using testing buckets");
-        var window_bucket_name = "aw-watcher-window-testing_" + this.host;
-        var afk_bucket_name = "aw-watcher-afk-testing_" + this.host;
+        this.window_bucket_name = "aw-watcher-window-testing_" + this.host;
+        this.afk_bucket_name = "aw-watcher-afk-testing_" + this.host;
       }
 
+      this.querySummary();
+      this.queryWindowTitles();
+      this.queryTimeline();
+    },
+
+    querySummary: function() {
       var summary_view_name = "windowactivity_summary@" + this.host;
-      var query = this.windowSummaryQuery(window_bucket_name, afk_bucket_name);
+      var query = this.windowSummaryQuery(this.window_bucket_name, this.afk_bucket_name);
       $CreateView.save({viewname: summary_view_name}, {'query': query}).then(
         (response) => { // Success
           if (response.status > 304){
-            this.errormsg = "Request error "+response.status+" at create view";
+            this.errorHandler(response);
+          } else {
+            this.queryViewSummary(summary_view_name);
           }
-          else {
-            var data = response.json();
-            this.queryView(summary_view_name);
-          }
-        },
-        (response) => { // Error
-          this.errormsg = "Request error "+response.status+" at create view";
-        }
-      );
+        }, this.errorHandler);
+    },
 
+    errorHandler: function(response) {
+      console.error(response);
+      this.errormsg = "Request error " + response.status + ". See F12 console for more info.";
+    },
+
+    queryTimeline: function() {
       var timeline_view_name = "windowactivity_timeline@"+this.host;
-      var query = this.windowTimelineQuery(window_bucket_name, afk_bucket_name);
+      var query = this.windowTimelineQuery(this.window_bucket_name, this.afk_bucket_name);
       $CreateView.save({viewname: timeline_view_name}, {'query': query}).then(
         (response) => { // Success
           if (response.status > 304){
-            this.errormsg = "Request error "+response.status+" at create view";
+            this.errorHandler(response);
+          } else {
+            this.queryViewTimeline(timeline_view_name);
           }
-          else {
-            var data = response.json();
-            this.queryView(timeline_view_name);
-          }
-        },
-        (response) => { // Error
-          this.errormsg = "Request error "+response.status+" at create view";
-        }
-      );
+        }, this.errorHandler);
     },
 
-    queryView: function(viewname){
-      var timeline_elem = document.getElementById("apptimeline-container")
-      timeline.set_status(timeline_elem, "Loading...");
+    todaysEvents: function(bucket_id) {
+      let today = moment(this.datestart);
+      return $Event.get({id: bucket_id, limit: -1,
+                         start: today.format(), end: today.add(1, "days").format()});
+    },
 
+    queryWindowTitles: function() {
+      var container = document.getElementById("windowtitles-container")
+      summary.set_status(container, "Loading...");
+
+      this.todaysEvents(this.window_bucket_name)
+        .then((response) => {
+          let events = response.json();
+
+          this.todaysEvents(this.afk_bucket_name).then((response) => {
+              let afkevents = response.json();
+              let filteredevents = event_parsing.filterAFKTime(events, afkevents);
+
+              let groups = _.groupBy(filteredevents, (o) => o.data.title);
+              let durations = _.mapValues(groups, (v, i) => _.reduce(v, (sum, o) => sum + o.duration, 0));
+
+              // Sort objects by duration
+              let durationPairs = _.sortBy(_.toPairs(durations), (a) => a[1]).reverse();
+              summary.updatePairs(container, _.take(durationPairs, this.numberOfWindowTitles));
+          })
+        }, this.errorHandler);
+    },
+
+    queryViewSummary: function(viewname){
       var appsummary_elem = document.getElementById("appsummary-container")
       summary.set_status(appsummary_elem, "Loading...");
 
       let today = moment(this.datestart);
-      $QueryView.get({"viewname": viewname,
-                      "limit": -1,
-                      "start": today.format(),
-                      "end": today.add(1, 'days').format()})
-        .then(
-        (response) => {
+      $QueryView.get({"viewname": viewname, "limit": -1,
+                      "start": today.format(), "end": today.add(1, 'days').format()})
+        .then((response) => {
           if (response.status > 304){
-            this.errormsg = "Server error "+response.status+" at view query";
-          }
-          else {
-            console.log(viewname)
+            this.errorHandler(request);
+          } else {
             var data = response.json();
             var chunks = data["chunks"];
-            var eventlist = data["eventlist"];
             this.duration = data["duration"];
             this.eventcount = data["eventcount"]+this.eventcount;
             if (chunks != undefined){
@@ -233,16 +261,32 @@ export default {
               var el = document.getElementById("appsummary-container")
               summary.update(el, appsummary);
             }
-            if (eventlist != undefined){
-              var apptimeline = event_parsing.parse_eventlist_by_apps(eventlist);
-              var el = document.getElementById("apptimeline-container")
-              timeline.update(el, apptimeline, this.duration);
-            }
           }
-        },
-        (response) => {
-          this.errormsg = "Request error "+response.status+" at view query";
-        });
+        }, this.errorHandler);
+    },
+
+    queryViewTimeline: function(viewname){
+      var timeline_elem = document.getElementById("apptimeline-container")
+      timeline.set_status(timeline_elem, "Loading...");
+
+      let today = moment(this.datestart);
+      $QueryView.get({"viewname": viewname,
+                      "limit": -1,
+                      "start": today.format(),
+                      "end": today.add(1, 'days').format()})
+        .then((response) => {
+          if (response.status > 304){
+            this.errorHandler(response);
+          } else {
+            var data = response.json();
+            var eventlist = data["eventlist"];
+            this.duration = data["duration"];
+            this.eventcount = data["eventcount"]+this.eventcount;
+            var apptimeline = event_parsing.parse_eventlist_by_apps(eventlist);
+            var el = document.getElementById("apptimeline-container")
+            timeline.update(el, apptimeline, this.duration);
+          }
+        }, this.errorHandler);
     },
 
 
