b46f6cfdba1164cf6a72783f356d3e0449b7afcd
Erik Bjäreholt <erik@bjareho.lt>
2017-08-23 16:11:09 +0200

    changed back to old color scheme

--- src/util/color.js
+++ src/util/color.js
@@ -1,9 +1,11 @@
+'use strict';
+
 const d3 = require("d3");
 
 // See here for examples:
 //   https://bl.ocks.org/pstuffa/3393ff2711a53975040077b7453781a9
 
-let scale = d3.scaleOrdinal(d3.schemeCategory20c);
+let scale = d3.scaleOrdinal(['#90CAF9', '#FFE082', '#EF9A9A', '#A5D6A7']);
 
 // Needed to prewarm the color table
 scale.domain([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]);
@@ -12,15 +14,17 @@ let customColors = {
     "afk": "#DDD",
     "not-afk": "#2D6",
 
-    "chrome": "#4885ed",   // Google Blue
+    "google-chrome": "#4885ed",   // Google Blue
     "chromium": "#4885ed", // Google Blue
     "spotify": "#1ED760",  // Spotify Green
     "alacritty": "#EA5"
-}
+};
 
 function hashcode(str){
-    var hash = 0;
-    if (str.length == 0) return hash;
+    let hash = 0;
+    if (str.length === 0) {
+      return hash;
+    }
     for (var i = 0; i < str.length; i++) {
         var character = str.charCodeAt(i);
         hash = ((hash<<5)-hash)+character;
