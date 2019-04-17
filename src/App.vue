d2657891fe3d318c7d96f3f7d05d2616dc3b1d6b
Erik Bjäreholt <erik@bjareho.lt>
2019-04-17 23:43:48 +0200

    added fixes for Android

--- src/App.vue
+++ src/App.vue
@@ -52,15 +52,11 @@ import 'vue-awesome/icons/desktop';
 
 import _ from 'lodash';
 
-// Set this to true to test Android behavior when on a desktop
-let testingAndroid = false;
-
 export default {
   data: function() {
     return {
       activityViews: [],
       info: {},
-      isAndroidApp: testingAndroid || navigator.userAgent.includes("Android") && navigator.userAgent.includes("wv"), // Checks for Android and WebView
     }
   },
 
@@ -70,7 +66,7 @@ export default {
         this.info = info;
       },
       (e) => {
-        console.error("Unable to connect:", e)
+        console.error("Unable to connect: ", e)
         this.info = {};
       }
     );
