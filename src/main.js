53b5792f27b16c1e3b2a562b5e7912fb05ac1add
Erik Bjäreholt <erik@bjareho.lt>
2019-10-18 23:25:56 +0200

    added category sunburst

--- src/main.js
+++ src/main.js
@@ -35,6 +35,7 @@ Vue.component('aw-header', () => import('./components/Header.vue'));
 Vue.component('aw-summary', () => import('./visualizations/Summary.vue'));
 Vue.component('aw-periodusage', () => import('./visualizations/PeriodUsage.vue'));
 Vue.component('aw-eventlist', () => import('./visualizations/EventList.vue'));
+Vue.component('aw-sunburst-categories', () => import('./visualizations/SunburstCategories.vue'));
 Vue.component('aw-sunburst-clock', () => import('./visualizations/SunburstClock.vue'));
 Vue.component('aw-timeline-inspect', () => import('./visualizations/TimelineInspect.vue'));
 Vue.component('aw-timeline', () => import('./visualizations/TimelineSimple.vue'));
--- src/views/activity/daily/ActivityDailySummary.vue
+++ src/views/activity/daily/ActivityDailySummary.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 div
-  div.row
+  div.row.mb-4
     div.col-md-4
       h5 Top Applications
       aw-summary(:fields="top_apps", :namefunc="e => e.data.app", :colorfunc="e => e.data.app", with_limit)
@@ -13,10 +13,7 @@ div
       h5 Top Browser Domains
       aw-summary(:fields="top_domains", :namefunc="e => e.data.$domain", :colorfunc="e => e.data.$domain", with_limit)
 
-  div.row
-    hr
-
-  div.row
+  div.row.mb-4
     div.col-md-4
       h5 Top categories
       div(v-if="top_categories")
@@ -26,15 +23,27 @@ div
       h5 Category Tree
       div(v-if="top_categories")
         aw-categorytree(:events="top_categories")
+
+  div.row.mb-4
+    div.col-md-6
+      h5 Category Sunburst
+      div(v-if="top_categories")
+        aw-sunburst-categories(:data="top_categories_hierarchy", style="height: 30em")
+
 </template>
 
 <script>
 import { get_day_period } from "~/util/time.js";
-import { loadClassesForQuery } from "~/util/classes";
+import { loadClassesForQuery, build_category_hierarchy } from "~/util/classes";
 import _ from 'lodash';
 
 import query from '~/queries.js';
 
+function pick_subname_as_name(c) {
+  c.name = c.subname;
+  c.children = c.children.map(pick_subname_as_name);
+  return c;
+}
 
 export default {
   name: "Activity",
@@ -45,6 +54,19 @@ export default {
     top_domains: function() { return this.$store.state.activity_daily.top_domains },
     //top_urls: function() { return this.$store.state.activity_daily.top_urls },
     browser_buckets: function() { return this.$store.state.activity_daily.browser_buckets },
+    top_categories_hierarchy: function() {
+      if(this.top_categories) {
+        console.log(this.top_categories);
+        let categories = this.top_categories.map(c => { return { name: c.data.$category, size: c.duration }; });
+        console.log(categories);
+
+        return {
+          name: "All",
+          children: build_category_hierarchy(categories).map(c => pick_subname_as_name(c))};
+      } else {
+        return null;
+      }
+    },
   },
 }
 </script>
