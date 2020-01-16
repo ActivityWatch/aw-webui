755c6f0f5a9b2de1f6bbc0a8933457e65c631f74
Daniele Scasciafratte <mte90net@gmail.com>
2020-01-16 14:59:10 +0100

    More software to categorize

--- src/util/classes.ts
+++ src/util/classes.ts
@@ -20,20 +20,20 @@ interface Category {
 }
 
 export const defaultCategories: Category[] = [
-  { name: ['Work'], rule: { type: 'regex', regex: 'Google Docs' } },
+  { name: ['Work'], rule: { type: 'regex', regex: 'Google Docs|libreoffice|ReText' } },
   {
     name: ['Work', 'Programming'],
-    rule: { type: 'regex', regex: 'GitHub|Stack Overflow' },
+    rule: { type: 'regex', regex: 'GitHub|Stack Overflow|BitBucket|Gitlab|vim|Spyder|kate' },
   },
   {
     name: ['Work', 'Programming', 'ActivityWatch'],
     rule: { type: 'regex', regex: 'ActivityWatch|aw-', ignore_case: true },
   },
   { name: ['Media', 'Games'], rule: { type: 'regex', regex: 'Minecraft|RimWorld' } },
-  { name: ['Media', 'Video'], rule: { type: 'regex', regex: 'YouTube|Plex' } },
+  { name: ['Media', 'Video'], rule: { type: 'regex', regex: 'YouTube|Plex|VLC' } },
   { name: ['Media', 'Social Media'], rule: { type: 'regex', regex: 'reddit|Facebook|Twitter|Instagram', ignore_case: true } },
-  { name: ['Comms', 'IM'], rule: { type: 'regex', regex: 'Messenger|Telegram|Signal|WhatsApp' } },
-  { name: ['Comms', 'Email'], rule: { type: 'regex', regex: 'Gmail' } },
+  { name: ['Comms', 'IM'], rule: { type: 'regex', regex: 'Messenger|Telegram|Signal|WhatsApp|Rambox|Slack' } },
+  { name: ['Comms', 'Email'], rule: { type: 'regex', regex: 'Gmail|Thunderbird' } },
 ];
 
 export function build_category_hierarchy(classes: Category[]): Category[] {
