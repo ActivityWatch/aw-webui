9742aebbf7dbb27000ab8a55a29818dc1b29fde7
Erik Bjäreholt <erik@bjareho.lt>
2021-02-07 18:38:30 +0100

    test: added basic vuex tests for categories

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,4 @@
 module.exports = {
-  // TODO: We should try to switch to the @vue/app preset, but right now it breaks everything
-  //presets: ['@vue/app'],
   presets: ['@vue/cli-plugin-babel/preset'],
   plugins: ['lodash', '@babel/plugin-syntax-dynamic-import'],
   comments: false,
--- jest.config.js
+++ jest.config.js
@@ -25,6 +25,10 @@ module.exports = {
       preset: 'ts-jest',
       testEnvironment: 'node',
       testMatch: ['**/test/**/*.test.node.js?(x)'],
+      transform: {
+        '^.+\\.js$': 'babel-jest',
+        '^.+\\.vue$': 'vue-jest',
+      },
       moduleNameMapper: {
         '^~/(.+)$': '<rootDir>/src/$1',
       },
