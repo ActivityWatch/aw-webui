938fc5fd063269e493d5393d8f1b9b264af4b268
Erik Bjäreholt <erik@bjareho.lt>
2021-03-26 11:38:38 +0100

    feat: improved style/layout of settings

---
+++ src/views/settings/ColorSettings.vue
@@ -0,0 +1,26 @@
+<template lang="pug">
+div.d-flex.justify-content-between
+  div
+    h5.mb-0 Use fallback colors
+    small
+      | Uses the old coloring style for some visualizations when uncategorized or no category color.
+  div
+    b-form-checkbox(v-model="useColorFallback", switch)
+</template>
+
+<script>
+export default {
+  name: 'ColorSettings',
+  data: () => {
+    return {
+      useColorFallback: localStorage.useColorFallback || true,
+    };
+  },
+  methods: {
+    set: function (useColorFallback) {
+      localStorage.useColorFallback = useColorFallback;
+      console.log('Set useColorFallback to ' + useColorFallback);
+    },
+  },
+};
+</script>
