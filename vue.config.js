cf4374dc362af4d49e286d45d4eb210c3fc68559
Michael Bianco <iloveitaly@gmail.com>
2021-05-03 16:49:13 -0600

    Pass AW_CLIENT_URL to the vue build

--- vue.config.js
+++ vue.config.js
@@ -28,6 +28,7 @@ module.exports = {
     plugins: [
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/),
       new webpack.DefinePlugin({
+        AW_CLIENT_URL: process.env.AW_CLIENT_URL,
         PRODUCTION: process.env.NODE_ENV === 'production',
       }),
       new CopyWebpackPlugin([{ from: 'static/', to: 'static' }]),
