29c3de04cf0ff43f00e467a60893f6f60eb9e84f
Michael Bianco <iloveitaly@gmail.com>
2021-05-03 16:49:32 -0600

    Allow the aw client URL to be specified via env vars in development
    This is helpful for testing a new build against your local production data

--- src/util/awclient.js
+++ src/util/awclient.js
@@ -5,10 +5,11 @@ let baseURL = '';
 // If running with `npm node dev`, use testing server as origin.
 // Works since CORS is enabled by default when running `aw-server --testing`.
 if (!PRODUCTION) {
-  const protocol = 'http';
-  const hostname = '127.0.0.1';
-  const port = '5666';
-  baseURL = protocol + '://' + hostname + ':' + port;
+  if (AW_CLIENT_URL) {
+    baseURL = AW_CLIENT_URL;
+  } else {
+    baseURL = 'http://127.0.0.1:5666';
+  }
 }
 
 const awc = new AWClient('aw-webui', { testing: !PRODUCTION, baseURL });
