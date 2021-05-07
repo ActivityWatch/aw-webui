0252ee93db1c8023dc264374cb761467d45d378c
Erik Bjäreholt <erik@bjareho.lt>
2021-05-07 16:07:28 +0200

    ci: better screenshots (with aw-fakedata)

--- test/e2e/screenshot.test.js
+++ test/e2e/screenshot.test.js
@@ -4,17 +4,15 @@
 
 import { Selector } from 'testcafe';
 
-fixture(`My fixture`).page(`http://localhost:27180/#/activity/erb-main2-arch`);
+fixture(`Home view`).page(`http://localhost:27180/`);
 
-test('Take a screenshot of the activity view', async t => {
-  await t
-    // TODO: Detect CI instead of never resizing
-    // For resizeWindow to work tests needs to run with a ICCCM/EWMH-compliant window manager
-    // Since CI just runs plain xvfb, it doesn't have that, so we don't.
-    // The resolution is the one used by the testcafe-action:
-    //   https://github.com/DevExpress/testcafe-action/blob/0989d5f8ad852d71298ce3b770442cdec309d479/index.js#L59-L60
-    //.resizeWindow(1280, 720)
-    .click('#load-demo');
+test('Screenshot the home view', async t => {
+  // TODO: Detect CI instead of never resizing
+  // For resizeWindow to work tests needs to run with a ICCCM/EWMH-compliant window manager
+  // Since CI just runs plain xvfb, it doesn't have that, so we don't.
+  // The resolution is the one used by the testcafe-action:
+  //   https://github.com/DevExpress/testcafe-action/blob/0989d5f8ad852d71298ce3b770442cdec309d479/index.js#L59-L60
+  // await t.resizeWindow(1280, 720);
 
   // Hide all devonly-elements
   const $hidedevonly = Selector('.hide-devonly');
@@ -22,14 +20,45 @@ test('Take a screenshot of the activity view', async t => {
     await t.click($hidedevonly.nth(i));
   }
 
-  // Closes the 'Network Error' message if running without aw-server
-  const $close = Selector('.close');
-  if (await $close.exists) {
-    await t.click('.close');
+  await t.takeScreenshot({
+    path: 'home.png',
+    fullPage: true,
+  });
+});
+
+fixture(`Activity view`).page(`http://localhost:27180/#/activity/fakedata`);
+
+test('Screenshot the activity view', async t => {
+  // Hide all devonly-elements
+  const $hidedevonly = Selector('.hide-devonly');
+  for (let i = 0; i < $hidedevonly.count; i++) {
+    await t.click($hidedevonly.nth(i));
   }
 
   await t.takeScreenshot({
     path: 'activity.png',
+    fullPage: true,
+  });
+});
+
+fixture(`Timeline view`).page(`http://localhost:27180/#/timeline`);
+
+test('Screenshot the timeline view', async t => {
+  // TODO: Detect CI instead of never resizing
+  // For resizeWindow to work tests needs to run with a ICCCM/EWMH-compliant window manager
+  // Since CI just runs plain xvfb, it doesn't have that, so we don't.
+  // The resolution is the one used by the testcafe-action:
+  //   https://github.com/DevExpress/testcafe-action/blob/0989d5f8ad852d71298ce3b770442cdec309d479/index.js#L59-L60
+  await t.resizeWindow(1280, 720);
+
+  // Hide all devonly-elements
+  const $hidedevonly = Selector('.hide-devonly');
+  for (let i = 0; i < $hidedevonly.count; i++) {
+    await t.click($hidedevonly.nth(i));
+  }
+
+  await t.takeScreenshot({
+    path: 'timeline.png',
     fullPage: true,
   });
 });
