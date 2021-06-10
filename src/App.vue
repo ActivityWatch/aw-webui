c1be8b581c6d6a0cc32965250ba8bc9f6ba8aa92
Erik Bjäreholt <erik@bjareho.lt>
2021-06-10 10:44:03 +0200

    fix: fixed comments left in review

--- src/App.vue
+++ src/App.vue
@@ -9,25 +9,34 @@ div#wrapper
         new-release-notification(v-if="isNewReleaseCheckEnabled")
         router-view
 
-  div.container(style="color: #555; font-size: 0.9em").px-0.px-md-2
-    div(style="float: left")
+  div.container(style="color: #555; font-size: 0.9em")
+    div.mb-2
       | Made with ❤ by the #[a(href="http://activitywatch.net/contributors/") ActivityWatch developers]
       div
         span.mt-2(v-show="info", style="color: #888; font-size: 0.8em")
           | #[b Version:] {{info.version}}.
           | #[b Host:] {{info.hostname}}
-    div.footer
-      a(href="https://twitter.com/ActivityWatchIt", target="_blank")
-        icon(name="brands/twitter")
-        | Twitter
-      | #[a(href="https://github.com/ActivityWatch/activitywatch/issues/new/choose") File a bug]
-      | #[a(href="https://forum.activitywatch.net/c/support") Join the Forum]
-      | #[a(href="https://forum.activitywatch.net/c/features") Vote on the forum]
+    div.pb-3.mb-3
+      div.float-left
+        a(href="https://twitter.com/ActivityWatchIt", target="_blank").mr-2
+          icon(name="brands/twitter")
+          | Twitter
+        a(href="https://github.com/ActivityWatch", target="_blank")
+          icon(name="brands/github")
+          | GitHub
+      div.float-right
+        a(href="https://github.com/ActivityWatch/activitywatch/issues/new/choose").mr-3
+          | Report a bug
+        a(href="https://forum.activitywatch.net/c/support").mr-3
+          | Ask for help
+        a(href="https://forum.activitywatch.net/c/features")
+          | Vote for features
 </template>
 
 <script>
 // only import the icons you use to reduce bundle size
 import 'vue-awesome/icons/brands/twitter';
+import 'vue-awesome/icons/brands/github';
 
 export default {
   data: function () {
