2f6c7e0e9364119f45239128287cfb52e423a2dd
Alexandre Murphy-Gonthier <murphyalexandre@gmail.com>
2021-08-12 20:47:05 -0400

    Brought back timeline viz and fixed heatmap for android.

--- src/util/datasets.ts
+++ src/util/datasets.ts
@@ -24,11 +24,6 @@ interface Dataset {
   type: string | undefined;
 }
 
-interface HeatmapDataset {
-  name: string;
-  data: number[];
-}
-
 function getColorWithOpacity(color: string, opacity: string): string {
   let newColor = color.split('');
   if (newColor[0] === '#') {
@@ -91,48 +86,6 @@ export function buildBarchartDataset(
       stack: 'combined',
       type: 'line',
     });
-
-    return ds;
-  } else {
-    return [];
-  }
-}
-
-export function buildHeatmapDataset(
-  $store: any,
-  data_by_period: HourlyData[]
-): HeatmapDataset[] {
-  const SEP = '>>>';
-  const data = data_by_period;
-  if (data) {
-    const category_names: Set<string> = new Set(
-      Object.values(data)
-        .map(result => {
-          return result.cat_events.map(e => e.data['$category'].join(SEP));
-        })
-        .flat()
-    );
-    const ds: HeatmapDataset[] = [...category_names]
-      .map(c_ => {
-        const c = $store.getters['categories/get_category'](c_.split(SEP));
-        if (c) {
-          const values = Object.values(data).map(results => {
-            const cat = results.cat_events.find(e => _.isEqual(e.data['$category'], c.name));
-            if (cat) return Math.round((cat.duration / (60 * 60)) * 1000) / 1000;
-            else return null;
-          });
-          return {
-            name: c.name.join(' > '),
-            data: values,
-          } as HeatmapDataset;
-        } else {
-          // FIXME: This shouldn't happen
-          // This may for example happen if one doesn't have an 'Uncategorized' category,
-          // as can happen when one upgrades from an old version where there wasn't one in the default classes.
-          console.error('missing category:', c_);
-        }
-      })
-      .filter(x => x);
 
     return ds;
   } else {
--- src/visualizations/Heatmap.vue
+++ src/visualizations/Heatmap.vue
@@ -1,21 +1,79 @@
 <template lang="pug">
-<div>
-  <apexchart type="heatmap" :options="options" :series="datasets" width="500" height="350"></apexchart>
-</div>
+div(style="overflow-x: scroll;", v-if="datasets !== null")
+  apexchart(type="heatmap", :options="options", :series="datasets")
 </template>
 
 <script>
+import _ from 'lodash';
+import moment from 'moment';
+
 export default {
   name: 'HeatmapChart',
   props: {
-    categories: { type: Array },
-    datasets: { type: Array },
+    buckets: { type: Array },
+    queriedInterval: { type: Array },
   },
   data: function() {
     return {
-      options: {
+      datasets: null,
+      options: null,
+    }
+  },
+  watch: {
+    buckets() {
+      this.generate();
+    },
+    queriedInterval() {
+      this.generate();
+    },
+  },
+  methods: {
+    generate() {
+      if (this.buckets.length === undefined || !this.queriedInterval) {
+        return;
+      }
+
+      // Build categories
+      const nbSep = 30;
+      const duration = moment.duration(this.queriedInterval[1].diff(this.queriedInterval[0])).asMinutes() / nbSep;
+      const categories = Array.from({ length: nbSep }, (el, i) => this.queriedInterval[0].clone().add(duration * i, 'minutes'));
+
+      // Build data for xaxis
+      const datasets = this.buckets
+        .filter(b => b.type === 'currentwindow')
+        .reduce((cur, bucket) => {
+          bucket.events.forEach(e => {
+            if (e.duration < 1) {
+              return;
+            }
+
+            if (!cur[e.data.app]) {
+              cur[e.data.app] = Array.from({ length: nbSep }, () => 0);
+            }
+
+            const foundIndex = categories.findIndex((c, i) => {
+              const timestamp = moment(e.timestamp);
+
+              if (i >= categories.length - 1) {
+                return true;
+              }
+
+              if (timestamp > c && timestamp < categories[i + 1]) {
+                return true;
+              }
+
+              return false;
+            });
+
+            cur[e.data.app][foundIndex] += e.duration;
+          });
+
+          return cur;
+        }, {});
+
+      this.options = {
         chart: {
-          width: 500,
+          width: "100%",
           height: 350,
           type: 'heatmap',
         },
@@ -27,10 +85,12 @@ export default {
           show: false,
         },
         xaxis: {
-          categories: this.categories,
+          categories: categories.map(c => c.format('HH:mm')),
         }
-      },
-    }
+      };
+
+      this.datasets = Object.entries(datasets).map(([key, value]) => ({ name: key, data: value }));
+    },
   },
 }
 </script>
