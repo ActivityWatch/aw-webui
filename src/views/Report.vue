e709c484beafd692fb8d63c94c9870db64f37fa1
Erik Bjäreholt <erik@bjareho.lt>
2022-04-26 17:23:58 +0200

    fix: progress on Report view

--- src/views/Report.vue
+++ src/views/Report.vue
@@ -20,11 +20,11 @@ div
   // select which categories, by having a form select and a "plus" button to include them
   b-input-group
     b-form-select(v-if="mode == 'category'" v-model="category" placeholder="Select a category")
-      b-form-select-option(v-for="category in categories" :key="category.id" :value="category.id")
-        | {{category.name.join(" > ")}}
+      b-form-select-option(v-for="cat in categories" :key="cat.id" :value="cat")
+        | {{cat.name.join(" > ")}}
     b-input(v-if="mode == 'custom'" v-model="pattern" v-on:keyup.enter="generate()" placeholder="Regex pattern to search for")
     b-input-group-append
-      b-button(type="button", @click="generate()" variant="success")
+      b-button(type="button", @click="generate()" variant="success" :disabled="!has_pattern")
         icon(name="search")
         | Generate
 
@@ -38,27 +38,17 @@ div
 
   div(v-if="show_options")
     h4 Options
-    div Hostname
-      select(v-model="hostname")
+    b-form-group(label="Hostname" label-cols=2)
+      b-form-select(v-model="hostname")
         option(v-for="hostname in Object.keys($store.getters['buckets/bucketsByHostname'])")
           | {{hostname}}
-    div Start: {{start.format()}}
-    div End: {{stop.format()}}
-    //div
-      label Use regex
-      input(type="checkbox" v-model="use_regex")
-    div
-      label Exclude time away from computer
-      input(type="checkbox" v-model="filter_afk")
-    //div.form-row
-      div.form-group.col-md-6
-        | Start
-        input.form-control(type="date", :max="today", v-model="startdate")
-      div.form-group.col-md-6
-        | End
-        input.form-control(type="date", :max="tomorrow", v-model="enddate")
-
-    div.form-inline
+    b-form-group(label="Start" label-cols=2)
+      b-form-datepicker(v-model="start")
+    b-form-group(label="Stop" label-cols=2)
+      b-form-datepicker(v-model="stop")
+    b-form-group(label="Toggles" label-cols=2)
+      b-form-checkbox(type="checkbox" v-model="filter_afk" label="Filter AFK" description="")
+        label Exclude time away from computer
 
   div(v-if="status == 'searching'")
     div #[icon(name="spinner" pulse)] Searching...
@@ -87,12 +77,16 @@ import 'vue-awesome/icons/spinner';
 import 'vue-awesome/icons/angle-double-down';
 import 'vue-awesome/icons/angle-double-up';
 
+const SEP = '>';
+
 export default {
   name: 'Report',
   data() {
     return {
       mode: 'category',
       pattern: '',
+      category: null,
+
       vis_method: 'eventlist',
       event_type: 'currentwindow',
       events: null,
@@ -104,13 +98,16 @@ export default {
       show_options: false,
       use_regex: true,
       filter_afk: true,
-      start: moment().subtract(1, 'day'),
-      stop: moment().add(1, 'day'),
+      start: moment().subtract(1, 'day').format('YYYY-MM-DD'),
+      stop: moment().add(1, 'day').format('YYYY-MM-DD'),
     };
   },
   computed: {
     categories: function () {
       return this.$store.state.categories.classes;
+    },
+    has_pattern: function () {
+      return this.mode === 'category' ? !!this.category : this.pattern.length > 0;
     },
   },
   mounted: async function () {
@@ -120,18 +117,30 @@ export default {
   },
   methods: {
     generate: async function () {
+      let classes;
+      if (this.mode === 'category') {
+        // Get the category and all subcategories
+        classes = this.categories
+          .filter(cat => cat.name.join(SEP).includes(this.category.name.join(SEP)))
+          .filter(cat => cat.rule.type === 'regex')
+          .map(cat => [cat.name, cat.rule]);
+      } else {
+        classes = [[['searched'], { type: 'regex', regex: this.pattern }]];
+      }
+      const filter_classes = classes.map(c => c[0]);
+
       // TODO: use full query (one per day/timeperiod) instead of canonicalEvents
       let query = canonicalEvents({
         bid_window: 'aw-watcher-window_' + this.hostname,
         bid_afk: 'aw-watcher-afk_' + this.hostname,
         filter_afk: this.filter_afk,
-        classes: [[['searched'], { type: 'regex', regex: this.pattern }]],
-        filter_classes: [['searched']],
+        classes,
+        filter_classes,
       });
       query += '; RETURN = events;';
 
       const query_array = query.split(';').map(s => s.trim() + ';');
-      const timeperiods = [this.start.format() + '/' + this.stop.format()];
+      const timeperiods = [moment(this.start).format() + '/' + moment(this.stop).format()];
       try {
         this.status = 'searching';
         const data = await this.$aw.query(timeperiods, query_array);
