11f2d2cf83aeb82699f021346d2e8d2233cbef18
Tatsuyuki Ishi <ishitatsuyuki@gmail.com>
2022-05-27 16:58:47 +0900

    Migrate to vue-chartjs v3, chart.js v4
    In order to further ease the migration to Vue 3, the decorator based component
    was rewritten in the normal object API.

--- src/visualizations/TimelineBarChart.vue
+++ src/visualizations/TimelineBarChart.vue
@@ -1,8 +1,12 @@
+<template>
+  <bar :chart-data="chartData" :chart-options="chartOptions" />
+</template>
+
 <script lang="ts">
 import _ from 'lodash';
 import { ChartOptions } from 'chart.js';
-import { Bar } from 'vue-chartjs';
-import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
+import 'chart.js/auto';
+import { Bar } from 'vue-chartjs/legacy';
 import { get_hour_offset } from '~/util/time';
 
 function hourToTick(hours: number): string {
@@ -19,61 +23,47 @@ function hourToTick(hours: number): string {
   }
 }
 
-@Component({
-  extends: Bar, // this is important to add the functionality to your component
-})
-export default class ChartTimelineBars extends Vue<Bar> {
-  constructor() {
-    super();
-  }
-
-  @Prop({
-    default: [
-      {
-        label: 'Total time',
-        backgroundColor: '#6699ff',
-        data: Array.from({ length: 40 }, () => Math.floor(Math.random() * 40)),
-      },
-    ],
-  })
-  datasets: Record<string, any>[];
-
-  @Prop({ default: 'day' })
-  resolution: 'day' | 'week' | 'month';
-
-  mounted() {
-    this.renderData();
-  }
-
-  @Watch('datasets')
-  onDatasetChange() {
-    this.renderData();
-  }
-
-  labels() {
-    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-    // @ts-ignore
-    const hourOffset = get_hour_offset();
-    if (this.resolution == 'day') {
-      return _.range(0, 24).map(h => `${(h + hourOffset) % 24}`);
-    } else if (this.resolution == 'week') {
-      // FIXME: In the future this will depend on a 'start of week' setting
-      return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
-    } else if (this.resolution == 'month') {
-      // FIXME: Needs access to the timeperiod start to know which month
-      const daysInMonth = 31;
-      return ['1st', '2nd', '3rd'].concat(_.range(4, daysInMonth + 1).map(d => `${d}th`));
-    } else if (this.resolution == 'year') {
-      return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    } else {
-      console.error('Invalid resolution');
-    }
-  }
-
-  yAxes() {
-    if (this.resolution == 'day') {
-      return [
+export default {
+  name: 'TimelineBarChart',
+  components: { Bar },
+  props: {
+    datasets: {
+      type: Array,
+      default: () => [
         {
+          label: 'Total time',
+          backgroundColor: '#6699ff',
+          data: Array.from({ length: 40 }, () => Math.floor(Math.random() * 40)),
+        },
+      ],
+    },
+    resolution: {
+      type: String,
+      default: 'day',
+    },
+  },
+  computed: {
+    labels() {
+      const hourOffset = get_hour_offset();
+      if (this.resolution == 'day') {
+        return _.range(0, 24).map(h => `${(h + hourOffset) % 24}`);
+      } else if (this.resolution == 'week') {
+        // FIXME: In the future this will depend on a 'start of week' setting
+        return ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+      } else if (this.resolution == 'month') {
+        // FIXME: Needs access to the timeperiod start to know which month
+        const daysInMonth = 31;
+        return ['1st', '2nd', '3rd'].concat(_.range(4, daysInMonth + 1).map(d => `${d}th`));
+      } else if (this.resolution == 'year') {
+        return ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+      } else {
+        console.error('Invalid resolution');
+        return [];
+      }
+    },
+    yAxis() {
+      if (this.resolution == 'day') {
+        return {
           stacked: true,
           ticks: {
             stepSize: 0.25,
@@ -81,51 +71,51 @@ export default class ChartTimelineBars extends Vue<Bar> {
             max: 1,
             callback: hourToTick,
           },
-        },
-      ];
-    } else {
-      return [
-        {
+        };
+      } else {
+        return {
           stacked: true,
           ticks: {
             stepSize: 1,
             min: 0,
             callback: hourToTick,
           },
+        };
+      }
+    },
+    chartData() {
+      return {
+        labels: this.labels,
+        datasets: _.sortBy(this.datasets, d => d.label),
+        title: {
+          display: true,
+          text: 'Timeline',
         },
-      ];
-    }
-  }
-
-  renderData() {
-    // Overwriting base render method with actual data.
-    const data = {
-      labels: this.labels(),
-      datasets: _.sortBy(this.datasets, d => d.label),
-      title: {
-        display: true,
-        text: 'Timeline',
-      },
-      responsive: true,
-      maintainAspectRatio: false,
-    };
-    const options = {
-      skipNull: true,
-      tooltips: {
-        mode: 'point',
-        intersect: false,
-      },
-      legend: {
-        display: false,
-      },
-      scales: {
-        xAxes: [{ stacked: true }],
-        yAxes: this.yAxes(),
-      },
-    } as ChartOptions;
-    this.renderChart(data, options);
-  }
-}
+        responsive: true,
+        maintainAspectRatio: false,
+      };
+    },
+    chartOptions(): ChartOptions {
+      return {
+        plugins: {
+          tooltip: {
+            mode: 'point',
+            intersect: false,
+          },
+          legend: {
+            display: false,
+          },
+        },
+        scales: {
+          x: {
+            stacked: true,
+          },
+          y: this.yAxes,
+        },
+      };
+    },
+  },
+};
 </script>
 
 <style></style>
