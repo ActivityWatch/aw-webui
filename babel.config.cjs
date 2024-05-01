7a61a82a49904fa9d4012cfb0caeacc68c604edc
Erik Bjäreholt <erik@bjareho.lt>
2024-05-01 17:16:01 +0200

    build(deps): removed @babel/plugin-syntax-dynamic-import (default in ES2020)

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,9 +1,5 @@
 module.exports = {
   presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    'lodash',
-    '@babel/plugin-syntax-dynamic-import',
-    '@babel/plugin-proposal-nullish-coalescing-operator',
-  ],
+  plugins: ['lodash', '@babel/plugin-proposal-nullish-coalescing-operator'],
   comments: false,
 };
