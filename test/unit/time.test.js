8c3f995f03a172fd5ea1f1658547573b48a36306
BelKed <66956532+BelKed@users.noreply.github.com>
2025-02-21 12:50:20 +0100

    fix: correct duration formatting logic

---
+++ test/unit/time.test.js
@@ -0,0 +1,23 @@
+import { seconds_to_duration } from '~/util/time';
+
+describe('seconds_to_duration', () => {
+  test('should format 8145 seconds as "2h 15m 45s"', () => {
+    expect(seconds_to_duration(8145)).toBe('2h 15m 45s');
+  });
+
+  test('should format 3630 seconds as "1h 0m 30s"', () => {
+    expect(seconds_to_duration(3630)).toBe('1h 0m 30s');
+  });
+
+  test('should format 1830 seconds as "30m 30s"', () => {
+    expect(seconds_to_duration(1830)).toBe('30m 30s');
+  });
+
+  test('should format 60 seconds as "1m 0s"', () => {
+    expect(seconds_to_duration(60)).toBe('1m 0s');
+  });
+
+  test('should format 30 seconds as "30s"', () => {
+    expect(seconds_to_duration(30)).toBe('30s');
+  });
+});
